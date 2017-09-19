7b23070ba364adb5645cfd0c3b4eff83825ae7ad
Michelle Cinello <m.cinello@gmail.com>
2017-09-19 16:37:23 -0400

    correct reverse name update

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,11 @@
+#Greeats backwards
 def greet_backwards(name)
-  return name.reverse
+  puts "Hey #{name.reverse}, how are you?"
+end
+
+greet_backwards(name)
+
+#Repeats twice
+def greet_backwards(name, n=2)
+  puts "Hey #{name.reverse * n}, how are you?"
 end
